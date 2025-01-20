c90f802fa8dbe6e75dc3a3f0d49d2e0945a6f4f7
sandeep <sandeepsanu1230@gmail.com>
2025-01-21 03:19:59 +0530

    updated recent projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -66,7 +66,7 @@ const RecentProjects = () => {
                   </div>
 
                   <div className="flex justify-center items-center">
-                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    <p className="flex lg:text-xl md:text-xs text-md text-purple">
                       Check Live Site
                     </p>
                     <FaLocationArrow className="ms-3" color="#CBACF9" />
